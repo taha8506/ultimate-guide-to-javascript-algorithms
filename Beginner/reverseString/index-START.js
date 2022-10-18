7d812110e981f02b3c04a4acd1bf381919d22955
taha8506 <taha8506@gmail.com>
2022-10-18 15:30:10 -0700

    two algorithsm completed

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,9 +4,49 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
-
+// builtin methods
 function reverseString(text) {
-    // Code goes here
+    return text.split("").reverse().join("")
+}
+
+// es6 + built in methods
+function reverseString(text) {
+    return [...text].reverse().join
+}
+
+//for loop method
+function reverseString(text) {
+    let result = ''
+
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i]
+    }
+    return result
+}
+
+// for of version
+function reverseString(text) {
+    let result = ""
+
+    for (let char of text) {
+        result = char + text
+    }
+
+    return result
+}
+
+// recursive way
+function reverseString(text) {
+    if (text === "") {
+        return ""
+    } else {
+        return reverseString(text.subsrtr(1)) + text[0]
+    }
+}
+
+//using reduce
+function reverseString(text) {
+    return text.split("").reduce((acc, char) => char + acc, "")
 }
 
 
--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,9 +3,38 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+//iterative way
 
 function vowelsCounter(text) {
-    // Code goes here
+    // initialize counter
+    let counter = 0
+
+    //loop through text to test if each character is a vowel
+    for (let letter of text.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            counter++
+        }
+    }
+    return counter
+}
+
+/*
+ Using Regular Expressions to count the number of vowels in a string of text.
+ */
+
+function vowelsCounter(text) {
+    // Search text with Regex and store all matching instances 
+    let matchingInstances = text.match(/[aeiou]/gi);
+
+    // Check if matching instances exist then calculate length
+    if (matchingInstances) {
+        // Return number of vowels
+        return matchingInstances.length
+    } else {
+        return 0
+    }
 }
 
 
